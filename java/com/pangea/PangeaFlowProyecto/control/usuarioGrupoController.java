6a3767ecbf485842a4775b7ec8287ee652ae94dd
Niuska Mora <niuskamora@gmail.com>
2013-09-09 15:53:17 -0430

    ahora si UsuarioGrupoRol

--- java/com/pangea/PangeaFlowProyecto/control/usuarioGrupoController.java
+++ java/com/pangea/PangeaFlowProyecto/control/usuarioGrupoController.java
@@ -32,85 +32,178 @@ public class usuarioGrupoController {
     private GestionDeUsuarios_Service service;
     @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_15362/CapaDeServicios/GestionDeGrupo.wsdl")
     private GestionDeGrupo_Service service_2;
-    private Usuario idusu;
-    private Grupo grupoSeleccionado, gru;
+    private Usuario idusu, usuarioGrupoSeleccionado;
+    private Grupo grupoSeleccionado;
     private Sesion sesion_actual;
     private List<Grupo> grupos;
-    private UsuarioGrupoRol grup, usuarioGrupoSeleccionado;
-    private List<UsuarioGrupoRol> grupo, grupoUsuarios, prueba;
-
-    public List<UsuarioGrupoRol> getPrueba() {
-        return prueba;
+    private UsuarioGrupoRol grup;
+    private List<UsuarioGrupoRol> grupo, grupoUsuarios;
+    private int indice;
+    private String idUsuario;
+    
+    /**
+     *
+     * @return
+     */
+    public Usuario getUsuarioGrupoSeleccionado() {
+        return usuarioGrupoSeleccionado;
     }
 
-    public void setPrueba(List<UsuarioGrupoRol> prueba) {
-        this.prueba = prueba;
+    /**
+     *
+     * @param usuarioGrupoSeleccionado
+     */
+    public void setUsuarioGrupoSeleccionado(Usuario usuarioGrupoSeleccionado) {
+        this.usuarioGrupoSeleccionado = usuarioGrupoSeleccionado;
     }
 
+    /**
+     *
+     * @return
+     */
     public Grupo getGrupoSeleccionado() {
         return grupoSeleccionado;
     }
 
+    /**
+     *
+     * @param grupoSeleccionado
+     */
     public void setGrupoSeleccionado(Grupo grupoSeleccionado) {
         this.grupoSeleccionado = grupoSeleccionado;
     }
 
-    public Grupo getGru() {
-        return gru;
+    /**
+     *
+     * @return
+     */
+    public Sesion getSesion_actual() {
+        return sesion_actual;
     }
 
-    public void setGru(Grupo gru) {
-        this.gru = gru;
+    /**
+     *
+     * @param sesion_actual
+     */
+    public void setSesion_actual(Sesion sesion_actual) {
+        this.sesion_actual = sesion_actual;
     }
 
+    /**
+     *
+     * @return
+     */
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    /**
+     *
+     * @param idUsuario
+     */
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getIndice() {
+        return indice;
+    }
+
+    /**
+     *
+     * @param indice
+     */
+    public void setIndice(int indice) {
+        this.indice = indice;
+    }
+
+    /**
+     *
+     * @return
+     */
     public List<UsuarioGrupoRol> getGrupoUsuarios() {
         return grupoUsuarios;
     }
 
+    /**
+     *
+     * @param grupoUsuarios
+     */
     public void setGrupoUsuarios(List<UsuarioGrupoRol> grupoUsuarios) {
         this.grupoUsuarios = grupoUsuarios;
     }
 
+    /**
+     *
+     * @return
+     */
     public UsuarioGrupoRol getGrup() {
         return grup;
     }
 
+    /**
+     *
+     * @param grup
+     */
     public void setGrup(UsuarioGrupoRol grup) {
         this.grup = grup;
     }
 
-    public UsuarioGrupoRol getUsuarioGrupoSeleccionado() {
-        return usuarioGrupoSeleccionado;
-    }
-
-    public void setUsuarioGrupoSeleccionado(UsuarioGrupoRol usuarioGrupoSeleccionado) {
-        this.usuarioGrupoSeleccionado = usuarioGrupoSeleccionado;
-    }
-
+    /**
+     *
+     * @return
+     */
     public List<UsuarioGrupoRol> getGrupo() {
         return grupo;
     }
 
+    /**
+     *
+     * @param grupo
+     */
     public void setGrupo(List<UsuarioGrupoRol> grupo) {
         this.grupo = grupo;
     }
     
-     public Usuario getIdusu() {
+     /**
+     *
+     * @return
+     */
+    public Usuario getIdusu() {
         return idusu;
     }
 
+    /**
+     *
+     * @param idusu
+     */
     public void setIdusu(Usuario idusu) {
         this.idusu = idusu;
     }
 
+    /**
+     *
+     * @return
+     */
     public List<Grupo> getGrupos() {
         return grupos;
     }
 
+    /**
+     *
+     * @param grupos
+     */
     public void setGrupos(List<Grupo> grupos) {
         this.grupos = grupos;
     }
 
+    /**
+     *
+     */
     @PostConstruct
     public void init() {
         idusu = new Usuario();
@@ -120,18 +213,17 @@ public class usuarioGrupoController {
         sesion_actual = (Sesion) (sesion.getAttribute("Sesion"));
         idusu= (Usuario) (sesion.getAttribute("Usuario"));
         
+        grupoSeleccionado = new Grupo();
+        
         grupos=listarGrupos();
         grupoSeleccionado = grupos.get(0);
         System.out.println("Grupoooo  "+grupoSeleccionado.getId());
         
-        prueba=listarUsuarioGrupoRol();
-        //usuarioGrupoSeleccionado=prueba.get(0);
-       //System.out.println("Usuarioooooo  "+usuarioGrupoSeleccionado.getIdUsuario().getId());
-        
-        grupo=this.listarGruposUsuarios(grupoSeleccionado);
-        grupoUsuarios = new ArrayList<UsuarioGrupoRol>();
+        grupo = new ArrayList<UsuarioGrupoRol>();
+        grupo=listarUsuariosGrupo(grupoSeleccionado);
         
         int j=0;
+        grupoUsuarios=new ArrayList<UsuarioGrupoRol>();
         
         if(grupo.isEmpty()){
             grupoUsuarios=null;
@@ -144,21 +236,24 @@ public class usuarioGrupoController {
                 grupoUsuarios.add(grup);
             }
             j++;
-            
         }
-        
-
     }
 
+    /**
+     *
+     * @param event
+     */
     public void onTabChange(TabChangeEvent event) {
        
         Grupo gSeleccionado = (Grupo) event.getData();
         grupoSeleccionado = gSeleccionado;
-        grupos=listarGrupos();
         
-        grupo=listarGruposUsuarios(grupoSeleccionado);
-        grupoUsuarios = new ArrayList<UsuarioGrupoRol>();
+        System.out.println("Grupoooo  "+grupoSeleccionado.getId());
         
+        grupo = new ArrayList<UsuarioGrupoRol>();
+        grupo=listarUsuariosGrupo(grupoSeleccionado);
+        
+        grupoUsuarios=new ArrayList<UsuarioGrupoRol>();
         
         if(grupo.isEmpty()){
             grupoUsuarios=null;
@@ -168,42 +263,54 @@ public class usuarioGrupoController {
         for (int i = 0; i < grupos.size(); i++) {
             if (grupos.get(i).getNombre().compareTo(gSeleccionado.getNombre()) == 0) {
                 g = grupos.get(i);
+                indice = i;
             }
-
         }
         
         int j=0;
-        
         while(grupo.size() > j){
             grup=grupo.get(j);
-            if(grup.getIdGrupo().getId().compareTo(grupoSeleccionado.getId())!=0){
-            }
-            else{
+            if(grup.getIdGrupo().getId().compareTo(grupoSeleccionado.getId())==0){
                 grupoUsuarios.add(grup);
             }
-            System.out.println("Usuariooooossss "+grupoUsuarios.get(j).getIdUsuario());
             j++;
             
         }
-        
     }
-
-   
+    
+    /**
+     *
+     */
+    public void verActividadesUsuario(){
+        
+        idUsuario=grup.getIdUsuario().getId();
+        System.out.println("Usuarioooooooo: "+idUsuario);
+        usuarioGrupoSeleccionado = new Usuario();
+        usuarioGrupoSeleccionado.setId(idUsuario);
+        
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = context.getExternalContext();
+        Object sessionInstancia = externalContext.getSession(true);
+        HttpSession httpSession = (HttpSession) sessionInstancia;
+        httpSession = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+        httpSession.setAttribute("IdUsuario", usuarioGrupoSeleccionado);
+        
+        try {
+            FacesContext contex = FacesContext.getCurrentInstance();
+            contex.getExternalContext().redirect("/PangeaFlowProyecto/faces/actividadesPorUsuario.xhtml");
+        } catch (Exception e) {
+            System.out.println("----------------------------Error---------------------------------" + e);
+        }
+    }
 
     private java.util.List<com.pangea.capadeservicios.servicios.Grupo> listarGrupos() {
         com.pangea.capadeservicios.servicios.GestionDeGrupo port = service_2.getGestionDeGrupoPort();
         return port.listarGrupos();
     }
 
-    private java.util.List<com.pangea.capadeservicios.servicios.UsuarioGrupoRol> listarGruposUsuarios(com.pangea.capadeservicios.servicios.Grupo grupo) {
+    private java.util.List<com.pangea.capadeservicios.servicios.UsuarioGrupoRol> listarUsuariosGrupo(com.pangea.capadeservicios.servicios.Grupo grupousuarios) {
         com.pangea.capadeservicios.servicios.GestionDeGrupo port = service_2.getGestionDeGrupoPort();
-        return port.listarGruposUsuarios(grupo);
+        return port.listarUsuariosGrupo(grupousuarios);
     }
 
-    private java.util.List<com.pangea.capadeservicios.servicios.UsuarioGrupoRol> listarUsuarioGrupoRol() {
-        com.pangea.capadeservicios.servicios.GestionDeGrupo port = service_2.getGestionDeGrupoPort();
-        return port.listarUsuarioGrupoRol();
-    }
-
-    
 }
